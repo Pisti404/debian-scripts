0abff3872aea643125710674e8d78e6a27d73419
AttilaM <59540584+aattilam@users.noreply.github.com>
2023-03-28 08:48:54 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -72,6 +72,12 @@ echo "Installing gnome and default software"
 apt install gnome-core libreoffice gnome-tweaks firefox-esr flatpak gnome-software-plugin-flatpak git nala -y
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 
+echo "Configuring Network Manager"
+
+sed -i '/managed=fasle/d' ./etc/NetworkManager/NetworkManager.conf
+echo "managed=true" >> /etc/NetworkManager/NetworkManager.conf
+
+
 echo "Installing firmware"
 cd
 git clone https://git.kernel.org/pub/scm/linux/kernel/git/firmware/linux-firmware.git/
